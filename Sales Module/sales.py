385513b9407cc37525e50703725081d668945748
Kha-Hau <91935541+Kha-Hau@users.noreply.github.com>
2021-11-04 22:34:58 +0800

    Added function to add sales record into database

--- Sales Module/sales.py
+++ Sales Module/sales.py
@@ -33,3 +33,32 @@ def execute_sql_file(filename):
         myCursor.execute(code)
 
     mydb.close()
+
+# Method to add an inventory item using the itemName, itemPrice, and itemQuantity received as parameters.
+# Return True if successful, else return False
+def add_sales(salesItem: str, salesAmount: float):
+    try:
+        # Create the INSERT code to be executed
+        sqlInsert = f"INSERT INTO Sales (salesItem, salesAmount) VALUES (%s, %s)"
+        data = (salesItem, salesAmount)
+
+        # Connect to database and execute the command
+        mydb = connect_db()
+        myCursor = mydb.cursor()
+        myCursor.execute(sqlInsert, data)
+        mydb.commit()
+
+        # Obtain the count of row affected
+        result = myCursor.rowcount
+        mydb.close()
+        
+        toReturn = False
+        # If row affected > 0, the insertion is successful. Return True on success, else return false
+        if(result > 0):
+            toReturn = True
+
+        return toReturn
+        
+    # If there is error, return False        
+    except Error:
+        return False
