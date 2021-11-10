e1ae98350da905a0f1236d664da80dc7564b2fe3
TimmyCFS <80512751+TimmyChin@users.noreply.github.com>
2021-11-10 11:57:28 +0800

    Updated the Report Module to generate sales report
    The Report Module can now generate the sales report into the system. Still pending changes made to grab from the Sales Table instead of the Manufacturer Table.

--- Report Module/report_module.py
+++ Report Module/report_module.py
@@ -1,5 +1,6 @@
 import manufacturer
 import mysql.connector
+import pandas as pd
 
 # Method to get the manufacturer details by ID
 def report_toCSV():
@@ -8,15 +9,26 @@ def report_toCSV():
 
         cursor = connection.cursor()
         sql_select_query = """select * from MANUFACTURER"""
+
+        # Testing
         cursor.execute(sql_select_query)
         records = cursor.fetchall()
 
         for i in range(len(records)):
+            print("\nRow Number", i)
             for j in range(len(records[i])):
                 print (records[i][j])
+
+        # Put the data into a dataframe
+        df = pd.read_sql(sql_select_query, connection)
+        print(df)
+
+        # Convert to CSV file format
+        df.to_csv("SalesReport.csv", index = False, header=True)
 
     except mysql.connector.Error as error:
         print("Unable to convert report to CSV: {}".format(error))
         return False
 
+# Runs the Report Module
 report_toCSV()
